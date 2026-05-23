bc2c4ab13ac038125d3e51c1e0571841f22e4b63
João Benetti <joaobenetti321@gmail.com>
2026-05-23 01:25:13 -0300

    adicionando tabela de bebidas, try execepts, soma total, carrinho e atualizando o banco de dados

--- main.py
+++ main.py
@@ -45,22 +45,79 @@ def limpar():
 # CARDAPIO
 # ====================================
 
-
 def mostrar_cardapio():
-    
+
+#PIZZAS
 
     conexao = conectar()
 
     cursor = conexao.cursor()
 
-    sql = "SELECT * FROM cardapio"
+    sql = "SELECT * FROM pizzas"
 
     cursor.execute(sql)
 
     pizzas = cursor.fetchall()
 
     print()
-    print('======= CARDÁPIO =======')
+    print('======= PIZZAS =======')
+
+    for pizza in pizzas:
+
+        print(
+            f'{pizza[0]} - '
+            f'{pizza[1]} - '
+            f'R${pizza[2]}'
+        )
+
+    fechar_conexao(conexao)
+
+#BEBIDAS
+
+    conexao = conectar()
+
+    cursor = conexao.cursor()
+
+    sql = "SELECT * FROM bebidas"
+
+    cursor.execute(sql)
+
+    bebidas = cursor.fetchall()
+
+    print()
+    print('======= BEBIDAS =======')
+
+    for bebida in bebidas:
+
+        print(
+            f'{bebida[0]} - '
+            f'{bebida[1]} - '
+            f'R${bebida[2]}'
+        )
+
+    fechar_conexao(conexao)
+
+
+
+# ====================================
+# PIZZAS
+# ====================================
+
+def mostrar_pizzas():
+    
+
+    conexao = conectar()
+
+    cursor = conexao.cursor()
+
+    sql = "SELECT * FROM pizzas"
+
+    cursor.execute(sql)
+
+    pizzas = cursor.fetchall()
+
+    print()
+    print('======= PIZZAS =======')
 
     for pizza in pizzas:
 
@@ -73,6 +130,36 @@ def mostrar_cardapio():
     fechar_conexao(conexao)
 
 
+
+#=================================
+# BEBIDAS
+# ================================
+
+def mostrar_bebidas():
+
+    conexao = conectar()
+
+    cursor = conexao.cursor()
+
+    sql = "SELECT * FROM bebidas"
+
+    cursor.execute(sql)
+
+    bebidas = cursor.fetchall()
+
+    print()
+    print('======= BEBIDAS =======')
+
+    for bebida in bebidas:
+
+        print(
+            f'{bebida[0]} - '
+            f'{bebida[1]} - '
+            f'R${bebida[2]}'
+        )
+
+    fechar_conexao(conexao)
+
 # ========================================
 # BUSCAR PIZZA
 # ========================================
@@ -83,7 +170,7 @@ def buscarPizza(id):
 
     cursor = conexao.cursor()
 
-    sql = "SELECT * FROM cardapio WHERE id = %s"
+    sql = "SELECT * FROM pizzas WHERE id = %s"
 
     valores = (id,)
 
@@ -97,25 +184,51 @@ def buscarPizza(id):
 
     input('\nPressione ENTER para voltar ao menu...')
 
+
+# ========================================
+# BUSCAR BEBIDA
+# ========================================
+
+def buscarBebida(id):
+
+    conexao = conectar()
+
+    cursor = conexao.cursor()
+
+    sql = "SELECT * FROM bebidas WHERE id = %s "
+
+    valores = (id,)
+
+    cursor.execute(sql, valores)
+
+    bebidas = cursor.fetchone()
+
+    fechar_conexao(conexao)
+
+    return bebidas
+    input('\nPressione ENTER para voltar menu...')
+
+
 # ========================================
 # ADICIONAR PEDIDO
 # ========================================
 
-def adicionarPedido(nome_cliente, pizza, preco):
+def adicionarPedido(nome_cliente, pizza, bebida, total):
 
     conexao = conectar()
 
     cursor = conexao.cursor()
 
     sql = """
-    INSERT INTO pedidos(nome_cliente, pizza, preco)
-    VALUES(%s, %s, %s)
+    INSERT INTO pedidos(nome_cliente, pizza, bebida, total)
+    VALUES(%s, %s, %s, %s)
     """
 
     valores = (
         nome_cliente,
         pizza,
-        preco
+        bebida,
+        total
     )
 
     cursor.execute(sql, valores)
@@ -163,7 +276,8 @@ def listarPedidos():
 ID: {pedido[0]}
 Cliente: {pedido[1]}
 Pizza: {pedido[2]}
-Preço: R${pedido[3]}
+Bebida: {pedido[3]}
+Preço total: R${pedido[4]}
                 '''
             )
    
@@ -176,6 +290,7 @@ Preço: R${pedido[3]}
 # ========================================
 
 def buscarPedido(id):
+    limpar()
 
     conexao = conectar()
 
@@ -192,19 +307,14 @@ def buscarPedido(id):
     fechar_conexao(conexao)
 
     return (pedido)
-  
-    
-    
-
-    
-    
+      
 
 
 # ========================================
 # ATUALIZAR PEDIDO
 # ========================================
 
-def atualizarPedido(id, nome_cliente, pizza, preco):
+def atualizarPedido(id, nome_cliente, pizza, bebida, total):
 
     conexao = conectar()
 
@@ -214,14 +324,16 @@ def atualizarPedido(id, nome_cliente, pizza, preco):
     UPDATE pedidos
     SET nome_cliente = %s,
         pizza = %s,
-        preco = %s
+        bebida = %s,
+        total = %s
     WHERE id = %s
     """
 
     valores = (
         nome_cliente,
         pizza,
-        preco,
+        bebida,
+        total,
         id
     )
 
@@ -229,9 +341,10 @@ def atualizarPedido(id, nome_cliente, pizza, preco):
 
     conexao.commit()
 
-    print('Pedido atualizado!')
+    print('\nPedido atualizado!')
 
     fechar_conexao(conexao)
+
     input('\nPressione ENTER para voltar ao menu...')
 
 
@@ -256,7 +369,7 @@ def removerPedido(id):
     print('Pedido removido!')
 
     fechar_conexao(conexao)
-    input('\nPressione ENTER para voltar ao menu...')
+    
 
 
 # ========================================
@@ -292,9 +405,11 @@ while(opcao != '0'):
         mostrar_cardapio()
         input('\nPressione ENTER para voltar ao menu...')
 
+   
     # ========================================
     # LISTAR PEDIDOS
     # ========================================
+
 
     elif(opcao == '2'):
 
@@ -307,47 +422,112 @@ while(opcao != '0'):
 
     elif(opcao == '3'):
 
-        print()
+        limpar()
+
         print('======= ADICIONAR PEDIDO =======')
 
         nome_cliente = input('Nome do cliente: ')
 
-        # MOSTRA CARDÁPIO
-        mostrar_cardapio()
+        carrinho = []
+        total = 0
 
-        # ESCOLHER PIZZA
-        pizza_id = int(input('Digite o ID da pizza: '))
+    # ================================
+    # PIZZAS
+    # ================================
 
-        # BUSCA NO BANCO
-        pizza = buscarPizza(pizza_id)
+        while True:
+            
+            mostrar_pizzas()
 
-        if pizza is None:
+            try:
 
-            print('Pizza não encontrada')
+                pizza_id = int(input('\nDigite o ID da pizza (0 para finalizar): '))
 
-        else:
+                if pizza_id == 0:
+                    break
 
-            pizza_nome = pizza[1]
-            pizza_preco = float(pizza[2])
+                pizza = buscarPizza(pizza_id)
 
-            print()
-            print('======= RESUMO =======')
+                if pizza:
 
-            print(f'Cliente: {nome_cliente}')
-            print(f'Pizza: {pizza_nome}')
-            print(f'Preço: R${pizza_preco}')
+                    carrinho.append(pizza[1])
+                    total += float(pizza[2])
+                    limpar()
 
-            adicionarPedido(
-                nome_cliente,
-                pizza_nome,
-                pizza_preco
-            )
+                    print(f'\n{pizza[1]} adicionada ao carrinho!')
+                    print(f'Total atual: R${total}')
+                    print('\n======= CARRINHO =======')
+                    for item in carrinho:
+                        print(f'- {item}')
+
+                    print(f'{pizza[1]} adicionada!')
+
+                else:
+
+                    print('Pizza não encontrada')
+
+            except ValueError:
+
+                print('Digite apenas números!')
+
+        # =========================
+        # BEBIDA
+        # =========================
+
+        bebida_nome = 'Sem bebida'
+
+        mostrar_bebidas()
+
+        try:
+
+            bebida_id = int(input('\nDigite o ID da bebida (0 para nenhuma): '))
+
+            if bebida_id != 0:
+
+                bebida = buscarBebida(bebida_id)
+
+                if bebida:
+
+                    bebida_nome = bebida[1]
+                    total += float(bebida[2])
+                    limpar()
+                    print(f'\n{pizza[1]} adicionada ao carrinho!')
+                    print(f'Total atual: R${total}')
+                    print('\n======= CARRINHO =======')
+                    for item in carrinho:
+                        print(f'- {item}')
+
+        except ValueError:
+
+            print('Digite apenas números!')
+
+        # =========================
+        # RESUMO
+        # =========================
+
+        pizzas_texto = ', '.join(carrinho)
+
+        print()
+        print('======= RESUMO =======')
+
+        print(f'Cliente: {nome_cliente}')
+        print(f'Pizzas: {pizzas_texto}')
+        print(f'Bebida: {bebida_nome}')
+        print(f'Total: R${total}')
+
+        adicionarPedido(
+            nome_cliente,
+            pizzas_texto,
+            bebida_nome,
+            total
+        )
 
     # ========================================
     # BUSCAR PEDIDO
     # ========================================
-
+         
     elif(opcao == '4'):
+        limpar()
 
         print()
         print('======= BUSCAR PEDIDO =======')
@@ -364,14 +544,15 @@ while(opcao != '0'):
 ID: {pedido[0]}
 Cliente: {pedido[1]}
 Pizza: {pedido[2]}
-Preço: R${pedido[3]}
+Bebida: {pedido[3]}
+Preço total: R${pedido[4]}
                 '''
             )
 
         else:
 
             print('Pedido não encontrado')
-            input('\nPressione ENTER para voltar ao menu...')
+        input('\nPressione ENTER para voltar ao menu...')
             
             
             
@@ -386,39 +567,53 @@ Preço: R${pedido[3]}
         print('======= ATUALIZAR PEDIDO =======')
 
         id = int(input('Digite o ID do pedido: '))
+
         pedido = buscarPedido(id)
 
         if pedido:
 
             nome_cliente = input('Novo nome do cliente: ')
 
-            mostrar_cardapio()
+            
 
-            pizza_id = int(input('Novo ID da pizza: '))
-
+            mostrar_pizzas()
+            pizza_id = int(input('\nNovo ID da pizza: '))
             pizza = buscarPizza(pizza_id)
 
-            if pizza:
+            limpar()
+
+            mostrar_bebidas()
+            print()
+            bebida_id = int(input('Novo ID da bebida: '))
+            bebida = buscarBebida(bebida_id)
+
+            if pizza and bebida:
 
                 pizza_nome = pizza[1]
                 pizza_preco = float(pizza[2])
+
+                bebida_nome = bebida[1]
+                bebida_preco = float(bebida[2])
+
+                total = pizza_preco + bebida_preco
 
                 atualizarPedido(
                     id,
                     nome_cliente,
                     pizza_nome,
-                    pizza_preco
+                    bebida_nome,
+                    total
                 )
 
             else:
 
-                print('Pizza não encontrada')
-                input('\nPressione ENTER para voltar ao menu...')
+                print('Pizza ou bebida não encontrada')
+            input('\nPressione ENTER para voltar ao menu...')
 
         else:
 
             print('Pedido não encontrado')
-            input('\nPressione ENTER para voltar ao menu...')
+        input('\nPressione ENTER para voltar ao menu...')
             
             
 
@@ -432,7 +627,7 @@ Preço: R${pedido[3]}
         print('======= REMOVER PEDIDO =======')
 
         listarPedidos()
-        id = int(input('Digite o ID do pedido: '))
+        id = int(input('Digite o ID para excluir o pedido: '))
 
         pedido = buscarPedido(id)
 
@@ -444,7 +639,7 @@ Preço: R${pedido[3]}
 
             print('Pedido não encontrado')
             print()
-            input('\nPressione ENTER para voltar ao menu...')
+        input('\nPressione ENTER para voltar ao menu...')
             
 
     # ========================================
@@ -458,4 +653,3 @@ Preço: R${pedido[3]}
 
 print()
 print('Sistema encerrado!')
-
